695209a1c6d7db0831119fdb2df79563b81fca42
cantinzing <cantinzing@163.com>
2017-10-11 23:22:02 +0800

    增加路由

---
+++ src/components/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div style="background:#eee;padding: 20px">
+        <Card :bordered="false">
+            <p slot="title">无边框标题</p>
+            <p>无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充无边框内容填充。</p>
+        </Card>
+        <div @click="showPage">点我切换路由</div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+          return {
+          }
+        },
+        methods: {
+          showPage: function(){
+            this.$router.push('/member_list')
+          }
+        },
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+
+// import index from '@/components/index/index' //路由正常加载
+const index = resolve => require(['../components/index/index'], resolve)// 路由懒加载
+const member_list = resolve => require(['../components/member/member_list'], resolve)
+
+
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    }
-  ]
+	    {
+		    path: '/',
+		    components: {
+		        main:index
+		    }
+		},
+	    { 
+	    	path: '/member_list',
+      		components: {
+        		main:member_list
+      		}
+    	},
+  	]
+
+
+  	
 })
