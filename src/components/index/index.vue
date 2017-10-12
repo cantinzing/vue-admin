9d7694e7d2308723d6a1467fd4be535d21ad4d8a
cantinzing <cantinzing@163.com>
2017-10-12 23:36:43 +0800

    修改主页布局样式

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,71 +1,8 @@
-<template>
-    <div class="layout">
-        <Row type="flex">
-            
-                <Menu active-name="1-2" theme="dark" width="240px" :open-names="['1']" accordion>
-                    <div class="layout-logo-left"></div>
-                    <Submenu name="1">
-                        <template slot="title">
-                            <Icon type="android-person" size="20"></Icon>
-                            会员管理
-                        </template>
-                        <MenuItem name="1-1">会员列表</MenuItem>
-                        <MenuItem name="1-2">选项 2</MenuItem>
-                        <MenuItem name="1-3">选项 3</MenuItem>
-                    </Submenu>
-                    <Submenu name="2">
-                        <template slot="title">
-                            <Icon type="ios-keypad" size="20"></Icon>
-                            导航二
-                        </template>
-                        <MenuItem name="2-1">选项 1</MenuItem>
-                        <MenuItem name="2-2">选项 2</MenuItem>
-                    </Submenu>
-                    <Submenu name="3">
-                        <template slot="title">
-                            <Icon type="ios-analytics" size="20"></Icon>
-                            导航三
-                        </template>
-                        <MenuItem name="3-1">选项 1</MenuItem>
-                        <MenuItem name="3-2">选项 2</MenuItem>
-                    </Submenu>
-                </Menu>
-            
-            <Col span="19">
-                <div class="layout-header"></div>
-                <div class="layout-breadcrumb">
-                    <Breadcrumb>
-                        <BreadcrumbItem href="#">首页</BreadcrumbItem>
-                        <BreadcrumbItem href="#">应用中心</BreadcrumbItem>
-                        <BreadcrumbItem>某应用</BreadcrumbItem>
-                    </Breadcrumb>
-                </div>
-                <div class="layout-content">
-                    <div class="layout-content-main">
-                      <!-- 路由 -->
-                      <router-view name="main"></router-view>
-                      <!-- 路由 -->
-                    </div>
-                </div>
-                <div class="layout-copy">
-                    2017-2030 &copy; cantinzing
-                </div>
-            </Col>
-        </Row>
-    </div>
-</template>
-<script>
-    export default {
-        
-    }
-</script>
-
 <style scoped>
     .layout{
         background: #f5f7f9;
         position: relative;
     }
-
     .ivu-row-flex{
       height: 100vh;
     }
@@ -83,7 +20,6 @@
         padding: 10px;
     }
     .layout-copy{
-        font-size: 15px;
         text-align: center;
         padding: 10px 0 20px;
         color: #9ea7b4;
@@ -103,13 +39,63 @@
         border-radius: 3px;
         margin: 15px auto;
     }
-
-    .layout-header{
-      width: calc(100vw - 240px);
-    }
-
-    .layout-content{
-      width: calc(100vw - 270px);
-      height: 82vh;
-    }
 </style>
+<template>
+    <div class="layout">
+        <Row type="flex">
+            <Col span="3" class="layout-menu-left">
+                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+                    <div class="layout-logo-left"></div>
+                    <Submenu name="1">
+                        <template slot="title">
+                            <Icon type="android-person" size="18"></Icon>
+                            会员管理
+                        </template>
+                        <MenuItem name="1-1">会员列表</MenuItem>
+                        <MenuItem name="1-2">选项 2</MenuItem>
+                        <MenuItem name="1-3">选项 3</MenuItem>
+                    </Submenu>
+                    <Submenu name="2">
+                        <template slot="title">
+                            <Icon type="ios-keypad" size="18"></Icon>
+                            导航二
+                        </template>
+                        <MenuItem name="2-1">选项 1</MenuItem>
+                        <MenuItem name="2-2">选项 2</MenuItem>
+                    </Submenu>
+                    <Submenu name="3">
+                        <template slot="title">
+                            <Icon type="ios-analytics" size="18"></Icon>
+                            导航三
+                        </template>
+                        <MenuItem name="3-1">选项 1</MenuItem>
+                        <MenuItem name="3-2">选项 2</MenuItem>
+                    </Submenu>
+                </Menu>
+            </Col>
+            <Col span="21">
+                <div class="layout-header"></div>
+                <div class="layout-breadcrumb">
+                    <Breadcrumb>
+                        <BreadcrumbItem href="#">首页</BreadcrumbItem>
+                        <BreadcrumbItem href="#">应用中心</BreadcrumbItem>
+                        <BreadcrumbItem>某应用</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                <div class="layout-content">
+                    <div class="layout-content-main">
+                      <router-view name="main"></router-view>
+                    </div>
+                </div>
+                <div class="layout-copy">
+                    2017-2030 &copy; cantinzing &reg;
+                </div>
+            </Col>
+        </Row>
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
